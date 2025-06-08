50a9de9bb1f456d75fe7a622b3cf3577514a2cfd
YassmeanA <yassmeanahmed23@gmail.com>
2025-06-08 17:54:49 +0200

    Update MusicPlaylist.js

--- MusicPlaylist.js
+++ MusicPlaylist.js
@@ -1140,7 +1140,20 @@ const handleEnd = () => {
     Item.classList.add("selected");
     Item.querySelector(".check").style.animation = "Scale 0.3s";
     count++;
-    updateDeleteUI(slideIndex);
+
+    document.querySelector(".delete-section").style.display = "flex";
+    document.querySelector(".delete-section span").innerHTML = `تم تحديد ${count} عناصر`;
+
+      Item.querySelector(".check").style.animation = "Scale 0.3s";
+
+      document.querySelectorAll(`.slide.num${SlideIndex} .item`).forEach(item => {
+        item.querySelector(".check").style.transition = "0.3s";
+        item.querySelector(".check").style.opacity = "1";
+        item.querySelector(".check").style.pointerEvents = "auto";
+        item.querySelector(".settings").style.display = "none";
+      });
+
+    
   }
 };
 
