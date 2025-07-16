3128bce01692c4c4a40084e1d837a5d21fbe1d6f
YassmeanA <yassmeanahmed23@gmail.com>
2025-07-16 16:18:23 +0200

    Update MusicPlaylist.js

--- MusicPlaylist.js
+++ MusicPlaylist.js
@@ -46,7 +46,7 @@ let audios = ["Song1.mp3", "Song2.mp3", "Song3.mp3", "Song4.mp3"];
 let Xs = ["X0", "X1", "X2", "X3"];
 
 
-//Remove loading animation after the page is full loaded
+//Remove loading animation after the page is fully loaded
 window.addEventListener("load",() => {
 setTimeout(() => {document.querySelector(".Loader").remove();},500);});
 
