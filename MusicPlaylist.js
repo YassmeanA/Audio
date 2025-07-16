7ef26ffe83f63d6f50ce109a57342cb2eb7df645
YassmeanA <yassmeanahmed23@gmail.com>
2025-07-16 16:11:02 +0200

    Update MusicPlaylist.js

--- MusicPlaylist.js
+++ MusicPlaylist.js
@@ -48,7 +48,7 @@ let Xs = ["X0", "X1", "X2", "X3"];
 
 //Remove loading animation after the page is full loaded
 window.addEventListener("load",() => {
-setTimeout(() => {document.querySelector(".Loader").remove();Pages.forEach(Page => {Page.style.opacity="1";});},500);});
+setTimeout(() => {document.querySelector(".Loader").remove();},500);});
 
 if ("ontouchstart" in document.documentElement) {document.body.classList.add("touch");document.body.classList.remove("mouse");}else{document.body.classList.add("mouse");document.body.classList.remove("touch");};
 
